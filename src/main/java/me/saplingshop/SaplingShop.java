a7f3122e69ab390a0f51c72570505a2dd170dd50
mizuki-lol <anoth3rst4r69@gmail.com>
2026-06-21 17:51:46 +0300

    Update SaplingShop.java

--- src/main/java/me/saplingshop/SaplingShop.java
+++ src/main/java/me/saplingshop/SaplingShop.java
@@ -1,9 +1,12 @@
 package me.saplingshop;
 
 import me.saplingshop.commands.ShopBuyCommand;
+import me.saplingshop.commands.ShopGenCommand;
 import me.saplingshop.commands.ShopSellCommand;
+import me.saplingshop.listeners.GeneratorListener;
 import me.saplingshop.listeners.InventoryClickListener;
 import me.saplingshop.managers.CoinManager;
+import me.saplingshop.managers.GeneratorManager;
 import me.saplingshop.managers.ShopBuyManager;
 import me.saplingshop.managers.ShopSellManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,17 +16,23 @@ public class SaplingShop extends JavaPlugin {
     private CoinManager coinManager;
     private ShopSellManager shopSellManager;
     private ShopBuyManager shopBuyManager;
+    private GeneratorManager generatorManager;
+    private GeneratorListener generatorListener;
 
     @Override
     public void onEnable() {
 
-        this.coinManager    = new CoinManager(this);
-        this.shopSellManager = new ShopSellManager(this);
-        this.shopBuyManager  = new ShopBuyManager(this);
+        this.coinManager      = new CoinManager(this);
+        this.shopSellManager  = new ShopSellManager(this);
+        this.shopBuyManager   = new ShopBuyManager(this);
+        this.generatorManager = new GeneratorManager(this);
 
         getCommand("shopsell").setExecutor(new ShopSellCommand(this));
         getCommand("shopbuy").setExecutor(new ShopBuyCommand(this));
+        getCommand("shopgen").setExecutor(new ShopGenCommand(this));
 
+        this.generatorListener = new GeneratorListener(this);
+        getServer().getPluginManager().registerEvents(generatorListener, this);
         getServer().getPluginManager().registerEvents(
                 new InventoryClickListener(this), this);
 
@@ -33,10 +42,12 @@ public class SaplingShop extends JavaPlugin {
     @Override
     public void onDisable() {
         if (coinManager != null) coinManager.saveData();
+        if (generatorListener != null) generatorListener.saveGenerators();
         getLogger().info("SaplingShop отключён.");
     }
 
-    public CoinManager getCoinManager()         { return coinManager; }
-    public ShopSellManager getShopSellManager() { return shopSellManager; }
-    public ShopBuyManager getShopBuyManager()   { return shopBuyManager; }
+    public CoinManager getCoinManager()           { return coinManager; }
+    public ShopSellManager getShopSellManager()   { return shopSellManager; }
+    public ShopBuyManager getShopBuyManager()     { return shopBuyManager; }
+    public GeneratorManager getGeneratorManager() { return generatorManager; }
 }
